dd629c413abea28cba0fde71f8abf0b8a49e06ac
Nikolaus Stoltzfus <nstoltzfus@ibacos.com>
2025-07-17 14:04:07 -0400

    Post testing updates

--- lando/statamic_install.sh
+++ lando/statamic_install.sh
@@ -1,3 +1,12 @@
 #!/bin/dash
 
-[ ! -f public/index.php ] && composer global require statamic/cli && statamic new statamic_starter
+if [ ! -f public/index.php ]
+then
+  composer global require statamic/cli
+  statamic new statamic_starter
+  rm statamic_starter/README.md
+  mv statamic_starter/* ./
+  mv statamic_starter/.editor* ./
+  mv statamic_starter/.env* ./
+  mv statamic_starter/.git* ./
+fi
